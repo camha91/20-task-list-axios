23a710204c42b6a456b164af5ed888f217f448c4
camha91 <crystalhanguyen@gmail.com>
2021-05-24 15:38:20 -0700

    Route for each pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Home/Header/Header";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
 
 function App() {
-    return <div className="app">Hello React</div>;
+    return (
+        <BrowserRouter>
+            <Header />
+            <Switch>
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
