60d9592bb94e935c1447f6dc29939d9205c69297
camha91 <crystalhanguyen@gmail.com>
2021-06-01 16:29:56 -0700

    Setup redux-saga middleware

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -2,10 +2,21 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import TaskListReducer from "./reducers/TaskListReducer";
 import reduxThunk from "redux-thunk";
 
+// middleWareSaga
+import createMiddlewareSaga from "redux-saga";
+import { rootSaga } from "./sagas/rootSaga";
+
+const middlewareSaga = createMiddlewareSaga();
+
 const rootReducer = combineReducers({
     TaskListReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+const store = createStore(
+    rootReducer,
+    applyMiddleware(reduxThunk, middlewareSaga)
+);
+
+middlewareSaga.run(rootSaga);
 
 export default store;
---
+++ src/redux/sagas/rootSaga.js
@@ -0,0 +1,3 @@
+export function* rootSaga() {
+    console.log("rootSaga");
+}
